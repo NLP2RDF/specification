39935f3e406082e0a8e676ab351b3ab9a119d0d5
kurzum <kurzum@github.com>
2013-08-17 10:18:36 +0200

    test commit

---
+++ publish.sh
@@ -0,0 +1,6 @@
+
+# sync run
+rsync -rav * nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/specification 
+# rsync -rav .htaccess nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/ontologies 
+
+
